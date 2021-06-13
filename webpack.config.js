99267c57e331672e62e1b55b13a720dd1d082791
jrodriguezdiazz <jorgedejesusrodriguezdiaz@gmail.com>
2021-06-12 22:54:05 -0400

    🔧 Add the initial configuration of Webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: "./src/index.js",
+  output: { path: path.resolve(__dirname, "dist"), filename: "main.js" },
+  resolve: {
+    extensions: [".js"],
+  },
+};
