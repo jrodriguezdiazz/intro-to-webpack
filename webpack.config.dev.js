42b1f1032d38e2ea10ae98e7248d422621815c09
jrodriguezdiazz <jorgedejesusrodriguezdiaz@gmail.com>
2021-07-01 15:19:14 -0400

    🔧 Add the configuration of dev server and fix the color of styl

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -75,4 +75,11 @@ module.exports = {
     }),
     new Dotenv(),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true
+  },
 };
