5bd94599754a3927c7e93d6cce67143d4624ce89
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2021-07-02 11:59:56 -0400

    🔧 Add cofiguration for JS and JSX in Webpack
    It is no longer necessary to import react by the new Babel configuration
    They were the HTML-Loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,28 @@ const path = require("path");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-    entry: ""
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    por: 3006,
+  },
 };
